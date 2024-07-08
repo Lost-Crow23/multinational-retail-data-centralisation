3d477fe07471f762b7b07991e244c61d373c582b
Lost-Crow <ruhal.appli@gmail.com>
2024-07-08 14:17:32 +0100

    nearly ms2 complete

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,75 @@
+from sqlalchemy.exc import SQLAlchemyError
+from database_utils import DatabaseConnector as db_connector
+import boto3
 import pandas as pd 
+import sqlalchemy as db
+import tabula as tb
+import requests
+import json
+import numpy as np
+from io import BytesIO
+
+
 
 class DataExtractor:
-    pass
+    def __init__(self, db_connector):
+        self.db_connector = db_connector
+
+    def read_data(self, connec):
+        inspector = db.inspect(connec)
+        tables = inspector.get_table_names()
+        print(tables)
+        return tables
+    
+    def read_rds_table(self, db_connector, table_name):
+        print("running the read_rds_table")
+        engine = db_connector.init_db_engine()
+        query = (f"SELECT * FROM  {table_name}")
+        data = pd.read_sql_table(query, engine)
+        df = pd.DataFrame(data)
+        print("done")
+        return df 
+    
+    def retrieve_pdf_data(self):
+        print("retrieving all the data...")
+        link = "https://data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+        card_details = pd.concat(tb.read_pdf(link, pages = 'all'), ignore_index = True)
+        print("done")
+        return (card_details)
+
+    def list_number_of_stores(self, my_endpoint, api_key):
+        print("running list_number_of_stores")
+        response = requests.get(my_endpoint, headers = api_key).content
+        result = json.loads(response)
+        result = result['number_of_stores']
+        print("done")
+        result(result)
+
+    def retrieve_stores_data(self, endpoint, num_stores, api_key):
+        print("running retreive_stores_data")
+        store_details = []
+        for stores in range(0, int(num_stores)):
+            response = requests.get(f"{endpoint}{stores}", headers = api_key).json()
+            store_details.append(pd.DataFrame(response, index = [np.NaN]))
+        store_details_df = pd.concat(store_details)
+        print("done")
+        return (store_details_df)
+    
+    def extract_from_s3(self, address):
+        print("running extract from s3")
+        client = boto3.client('s3')
+        if "s3://" in address:
+             split_address = address.replace("s3://", "").split("/", 1)
+        elif "https://" in address:
+             split_address = address.replace("https://", "").split("/", 1)
+        bucket_name = 'data-handling-public'
+        s3key_path = "/".join(split_address[1:])
+        data = client.get_object(Bucket = bucket_name, Key = s3key_path)
+        data = data['Body'].read()
+
+        if 'csv' in s3key_path:
+            df = pd.read_csv(BytesIO(data))
+        elif 'json' in s3key_path:
+            df = pd.read_json(BytesIO(data))
+        print('done')
+        return df
--- database_utils.py
+++ database_utils.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 import yaml 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
+import psycopg2
 
 class DatabaseConnector():
     
+    def __init__(self):
+        pass
+
     def read_db_creds(self):
         with open ("./db_creds.yaml", "r") as stream:
             creds = yaml.safe_load(stream)
@@ -12,12 +16,35 @@ class DatabaseConnector():
     def init_db_engine(self):
         creds = self.read_db_creds()
         # engine = create_engine(creds)
-        engine = create_engine(f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
-        return engine 
+        connec = create_engine(f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
+        cur = connec.cursor()
+        return cur
+
+    def list_db_tables(self):
+        cur = self.establish.connec()    
+        # with self.engine.connec() as connection:
+        query = ("""SELECT table_name FROM information_schema.tables 
+                 WHERE table_schema = 'public'""")
+        cur.execute(query)
+        for table in cur.fetchall():
+            print(table)
+        return table
+    
+    def upload_to_db(self, df, table_name, yaml_file):
+        with open("./db_creds_local.yaml") as db_creds_local:
+            creds = yaml.safe_load(db_creds_local)
+        engine = create_engine (f"{'postgresql'}+{'psycopg2'}://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['dbname']}")
+        engine.connect()
+        df.to_sql(table_name, engine, if_exists = 'replace')
+        print('Upload Done')
 
 
-connec = DatabaseConnector()
-connec.read_db_creds()
+
+# if __name__ == '__main__':
+#     connec = DatabaseConnector()
+#     connec.read_db_creds()
+#     engine = connec.init_db_engine()
+#     connec.list_db_tables()
 
 
     
